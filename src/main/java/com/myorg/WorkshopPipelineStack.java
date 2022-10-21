38f997fcd613601c72a151ef86de6178e7130aec
Cristian Flores <ceflores11@gmail.com>
2022-10-21 15:31:25 -0700

    testing cdk pipeline

---
+++ src/main/java/com/myorg/WorkshopPipelineStack.java
@@ -0,0 +1,24 @@
+package com.myorg;
+
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+
+import software.amazon.awscdk.services.codecommit.Repository;
+
+public class WorkshopPipelineStack extends Stack {
+    public WorkshopPipelineStack(final Construct parent, final String id) {
+        this(parent, id, null);
+    }
+
+    public WorkshopPipelineStack(final Construct parent, final String id, final StackProps props) {
+        super(parent, id, props);
+
+        // This creates a new CodeCommit repository called 'WorkshopRepo'
+        final Repository repo = Repository.Builder.create(this, "WorkshopRepo")
+                .repositoryName("WorkshopRepo")
+                .build();
+
+        // Pipeline code goes here
+    }
+}
