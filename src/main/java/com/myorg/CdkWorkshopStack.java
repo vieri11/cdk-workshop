4e624a0ea1a56601bbaee4a32f731cc154fe1736
Cristian Flores <ceflores11@gmail.com>
2022-10-21 17:05:45 -0700

    MESSAGE

--- src/main/java/com/myorg/CdkWorkshopStack.java
+++ src/main/java/com/myorg/CdkWorkshopStack.java
@@ -1,15 +1,22 @@
 package com.myorg;
 
 import io.github.cdklabs.dynamotableviewer.TableViewer;
+
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.CfnOutput;
+
 import software.amazon.awscdk.services.apigateway.LambdaRestApi;
 import software.amazon.awscdk.services.lambda.Code;
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.Runtime;
-import software.constructs.Construct;
-import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
 
 public class CdkWorkshopStack extends Stack {
+
+    public final CfnOutput hcViewerUrl;
+    public final CfnOutput hcEndpoint;
+
     public CdkWorkshopStack(final Construct parent, final String id) {
         this(parent, id, null);
     }
@@ -29,19 +36,23 @@ public class CdkWorkshopStack extends Stack {
                 .downstream(hello)
                 .build());
 
-        // Defines API Gateway REST API resource backed by hello function 'hello.js'
-        // API Gateway exposes a public HTTP endpoint that anyone on the internet can hit with
-        // an http client such as curl or a web browser
-        LambdaRestApi.Builder.create(this, "Endpoint")
+        // Defines an API Gateway REST API resource backed by our "hello" function
+        final LambdaRestApi gateway = LambdaRestApi.Builder.create(this, "Endpoint")
                 .handler(helloWithCounter.getHandler())
                 .build();
 
         // Defines a viewer for the HitCounts table
-        TableViewer.Builder.create(this, "ViewerHitCount")
+        final TableViewer tv = TableViewer.Builder.create(this, "ViewerHitCount")
                 .title("Hello Hits")
                 .table(helloWithCounter.getTable())
-                .sortBy("hits")
                 .build();
 
+        hcViewerUrl = CfnOutput.Builder.create(this, "TableViewerUrl")
+                .value(tv.getEndpoint())
+                .build();
+
+        hcEndpoint = CfnOutput.Builder.create(this, "GatewayUrl")
+                .value(gateway.getUrl())
+                .build();
     }
 }
